76a62f940c63f99953d486227b3cc4650726a181
Shubham Shekhar <shubhamshekhar089@gmail.com>
2017-08-27 02:59:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 //counter code
-var button = document.getElementById("counter");
+var button = document.getElementById('counter');
 button.oncLick = function() {
 
 //Create A Request To Counter Endpoint
-var request = new XMLhttpRequest();
+var request = new XMLHttpRequest();
 //Capture The Response And Store In HTML
 request.onreadystatechange = function() {
 if (request.readyState === XMLHttpRequest.DONE) 
@@ -12,32 +12,32 @@ if (request.readyState === XMLHttpRequest.DONE)
 if (request.status === 200) 
 { 
 var counter= request.responseText; 
-var span = document.getElementById("count");
+var span = document.getElementById('count');
 span.innerHTML = counter.toString();
 }
 
   }
 };
 //Make A Request
-request.open('GET', 'http://shubhamshekhar089.imad.hasura-app.io', true);
+request.open('GET', 'http://shubhamshekhar089.imad.hasura-app.io/counter', true);
 request.send(null);
 };
 
 //submit name
-var nameInput=document.getElementById('name');
-var name=nameInput.value;
-var submit= document.grtElementById('submit_btn');
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
 submit.onclick=function(){
   //make the request to the server and send the name
   
   //capture a list of names and render it as a list
-  var names=['name1','name2','name3','name4'];
-  var list='';
-  for(var i=0;i< names.length;i++){
+  var names = ['name1','name2','name3','name4'];
+  var list = '';
+  for(var i=0; i< names.length; i++){
       list += '<li>' + names[i] + '</li>' ;
   }
-  var ul=document.getElementById('namelist');
-  ul.innerHTML=list;
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
 };
 
 
