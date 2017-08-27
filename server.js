4824e3e8b98dbec55f18eee9f68a10ec1c4ec871
Shubham Shekhar <shubhamshekhar089@gmail.com>
2017-08-27 23:45:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
-app.get('/articleName', function (req, res) {
+app.get('/:articleName', function (req, res) {
     //article name will be article-one
     //articles[articleName]=={} content object for article-one
     var articleName = req.params.articleName;
